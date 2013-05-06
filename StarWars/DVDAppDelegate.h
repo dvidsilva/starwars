6392ba5e29d810e962c4de96229f10e4123d1532
Dvid Silva <contacto@dvidsilva.com>
2013-05-06 13:38:38 -0500

    Initial Commit

---
+++ StarWars/DVDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DVDAppDelegate.h
+//  StarWars
+//
+//  Created by Dvid Silva on 5/6/13.
+//  Copyright (c) 2013 Dvid Silva. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DVDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ StarWars/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  StarWars
+//
+//  Created by Dvid Silva on 5/6/13.
+//  Copyright (c) 2013 Dvid Silva. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DVDAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DVDAppDelegate class]));
+    }
+}
